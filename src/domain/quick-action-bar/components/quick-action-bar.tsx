565e334fab70f93be680b3cf709b86bd466b38bb
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2026-06-07 14:46:02 -0300

    dia dos namorados

--- src/domain/quick-action-bar/components/quick-action-bar.tsx
+++ src/domain/quick-action-bar/components/quick-action-bar.tsx
@@ -1,4 +1,4 @@
-import { BellRing, Bike, Hand, HeartHandshake, Proportions, Scissors, Siren, SquareMenu, Watch } from 'lucide-react';
+import { BellRing, Bike, Hand, Heart, HeartHandshake, Proportions, Scissors, Siren, SquareMenu, Watch } from 'lucide-react';
 import ButtonQuickAction from '../../template-messages/components/button-quick-action';
 import { cortarPizza, filaWhatsApp, linKCardapioText, pixText, pizzaSize } from '../../../db/message-templates';
 import PixSvgIcon from '../../template-messages/components/pix-svg-icon';
@@ -36,6 +36,15 @@ export default function QuickActionBar() {
         <BellRing size={14} />
       </ButtonQuickAction>
 
+      <ButtonQuickAction
+        templateText={{
+          title: 'Combo Dia dos Namorados',
+          content: '*COMBO ESPECIAL DIA DOS NAMORADOS*\nGaranta sua reserva antecipada e aproveite o melhor valor.\n\n🍕 Inclui:\n* 1 Pizza Valentino (Média)\n* 1 Pizza Mon Cherry (Pequena - doce)\n\n💰 Valores\n* Reserva antecipada: *R$ 159,90*\n* No Dia dos Namorados: *R$ 179,90*\n\n🏷 Economia\n* Reservando antecipadamente, você *economiza R$ 30,00*\n\n⚠️ Devido à alta procura na data, as reservas são limitadas e sujeitas à disponibilidade.\n\n* Combo com composição fixa.\n* Não é possível realizar alterações ou substituições.\n\n🍕 Todo o cardápio também estará disponível para pedidos individuais.\n\n🚨 Não deixe para a última hora! *As vagas para reserva são limitadas* e costumam se esgotar rapidamente. Garanta agora seu combo pelo valor promocional, economize R$ 30,00 e assegure uma experiência inesquecível!'
+        }}
+        showToast={true}
+      >
+        <Heart size={14} />
+      </ButtonQuickAction>
       <ButtonQuickAction
         templateText={pizzaSize()}
         showToast={true}
